4b96d27f07db999ca8f916e39a786cd94538b27d
wupeng <836720919@qq.com>
2024-11-24 21:26:13 +0800

    代码优化及增加注释

--- utils/fileHandle.js
+++ utils/fileHandle.js
@@ -22,7 +22,12 @@ const readJSON = async (jsonFile) => await fs.readJSON(jsonFile)
 // 错误提示
 const errorTip = `.iconfontrc不存在，请使用iconfont-manager init <phoneNumber> <password>进行初始化或在${homedir}目录下新建该文件`
 
-// 解析用户目录下的.iconfontrc文件，获取所有的图标库信息
+/** 解析.iconfontrc文件，获取图标库信息
+ * 如果项目目录下有.iconfontrc文件，则优先读取该文件，否则读取系统用户目录下的.iconfontrc文件
+ * 注意：因为.iconfontrc文件涉及到账号密码，所以一般建议放在系统用户目录下！！！
+ * 1.若账号密码是公开、公共使用的，可以直接放在项目目录下并将这个文件提交到版本库
+ * 2.若团队成员都有自己的账号密码，应该在.ignconfontrc文件中进行标注，提交代码时不要提交这个文件
+*/
 const readConfig = async () => {
   let config = []
   if(!isExist(joinPath(homedir, '.iconfontrc'))) {
@@ -37,7 +42,7 @@ const readConfig = async () => {
   return config
 }
 
-// 将新的图标库信息写入用户目录下的.iconfontrc文件
+// 将新的图标库信息写入.iconfontrc文件
 const writeConfig = async (content) => {
   if(!isExist(joinPath(homedir, '.iconfontrc'))) {
     console.error(errorTip);
